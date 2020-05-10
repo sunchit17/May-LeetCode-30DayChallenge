38099013adbb22c906cecae030c95a6f53d41d94
Sunchit Lakhanpal <43404214+sunchit17@users.noreply.github.com>
2020-05-10 23:12:03 +0530

    Create Town_Judge(day 10).java

---
+++ Town_Judge(day 10).java
@@ -0,0 +1,46 @@
+/*
+In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+Example 1:
+Input: N = 2, trust = [[1,2]]
+Output: 2
+
+Example 2:
+Input: N = 3, trust = [[1,3],[2,3]]
+Output: 3
+
+Example 3:
+Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+*/
+
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        
+        int flag=-1;
+        int[] arr = new int[N+1];
+        int[] arr2 = new int[N+1];
+        
+        for(int i=0;i<trust.length;i++)
+        {
+            int[] val = trust[i];
+            arr[val[0]]++;
+            arr2[val[1]]++;
+        }
+        for(int i=1;i<=N;i++)
+        {
+            if(arr[i]==0&&arr2[i]==N-1)
+                flag=i;
+            }
+        return flag;    
+    }
+}
